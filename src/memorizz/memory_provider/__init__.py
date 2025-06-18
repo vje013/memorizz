1c007d8f679650d396242b579a60aefd52a2b963
RichmondAlake <richmond.alake@gmail.com>
2025-06-18 03:46:04 +0100

    removed pg mentions

--- src/memorizz/memory_provider/__init__.py
+++ src/memorizz/memory_provider/__init__.py
@@ -7,7 +7,3 @@ __all__ = [
     'MongoDBProvider',
     'MemoryType'
 ]
-
-# Only add PostgreSQL classes to __all__ if they're available
-if _postgresql_available:
-    __all__.extend(['PostgreSQLProvider', 'PostgreSQLConfig'])
